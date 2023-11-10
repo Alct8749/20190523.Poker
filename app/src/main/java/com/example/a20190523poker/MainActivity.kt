e602da497666a985deda8aea5be5f5f51cd4ff2c
none123 <76775605+TIM-ai-coder@users.noreply.github.com>
2023-11-11 08:19:09 +0900

    ,

--- app/src/main/java/com/example/a20190523poker/MainActivity.kt
+++ app/src/main/java/com/example/a20190523poker/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         set_image() // 이미지 설정
 
+
         main.btnShuffle.setOnClickListener {
         model.shuffle()
         }
@@ -26,6 +27,13 @@ class MainActivity : AppCompatActivity() {
 
         model = ViewModelProvider(this)[CardDealerViewModel::class.java]
         model.cards.observe(this, Observer {
+
+            val cardList: List<Int> = it.toList()
+
+            val pokerHand = determinePokerHand(cardList)
+            main.jokboText.text = "포커 족보: $pokerHand"
+
+
             //조커모양으로 초기화
             val res = IntArray(5)
             for (i in it.indices) {
@@ -51,6 +59,57 @@ class MainActivity : AppCompatActivity() {
             main.card22.setImageResource(res[4])
         })
     }
+    fun determinePokerHand(cards: List<Int>): String {
+        val counts = mutableMapOf<Int, Int>()
+
+        for (card in cards) {
+            val rank = card % 13
+            counts[rank] = counts.getOrDefault(rank, 0) + 1
+        }
+
+        val numPairs = counts.values.count { it == 2 }
+        val numThreeOfAKind = counts.values.count { it == 3 }
+        val numFourOfAKind = counts.values.count { it == 4 }
+
+        val hasFlush = cards.all { it / 13 == cards[0] / 13 }
+
+        val isStraight = counts.keys.sorted().let {
+            it.size == 5 && (it[4] - it[0] == 4 || it == listOf(0, 9, 10, 11, 12))
+        }
+
+        // 백 스트레이트 플러시 확인
+        val hasBackStraightFlush = counts.keys.sorted().let {
+            it.size == 5 && it == listOf(0, 1, 2, 3, 4)
+        }
+
+        // 로열 스트레이트 플러시 확인
+        val hasRoyalStraightFlush = counts.keys.sorted().let {
+            it.size == 5 && it == listOf(0, 9, 10, 11, 12)
+        }
+
+        // 백 스트레이트 확인
+        val hasBackStraight = counts.keys.sorted() == listOf(0, 1, 2, 3, 4)
+
+        // 마운틴 확인
+        val hasMountain = counts.keys.sorted() == listOf(0, 9, 10, 11, 12)
+
+        return when {
+            isStraight && hasFlush -> "스트레이트 플러시"
+            hasBackStraightFlush -> "백 스트레이트 플러시"
+            numFourOfAKind == 4 -> "포카드"
+            numThreeOfAKind == 3 && numPairs == 2 -> "풀하우스"
+            hasFlush -> "플러시"
+            isStraight -> "스트레이트"
+            numThreeOfAKind == 3 -> "트리플"
+            numPairs == 2 -> "투 페어"
+            numPairs == 1 -> "원 페어"
+            hasRoyalStraightFlush -> "로열 스트레이트 플러시"
+            hasBackStraight -> "백 스트레이트"
+            hasMountain -> "마운틴"
+            else -> "탑"
+        }
+    }
+
 
 
 
@@ -90,5 +149,7 @@ class MainActivity : AppCompatActivity() {
 
         return "c_${number}_of_${shape}"
     }
+
+
 
 }
